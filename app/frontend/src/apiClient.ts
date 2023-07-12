926ae56a92a83c72b9f51c7546b62ad8889a7052
Maksym Kalinchyk <95409102+MaksymKalinchyk@users.noreply.github.com>
2023-07-12 16:26:00 +0200

    update baseurl

--- app/frontend/src/apiClient.ts
+++ app/frontend/src/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://75.119.156.123:3000/api",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
